4c7f209e9d055831b6fdb09e85cd9e684216b366
abdulrehmanawan786-dev <abdulrehmanawan786@gmail.com>
2026-07-01 21:26:43 +0500

    Update scheduler.js

--- server/scheduler.js
+++ server/scheduler.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const pool = require('./config/db');
 const { sendWhatsAppTemplate } = require('./utils/whatsapp');
 
-cron.schedule('24 21 * * *', async () => {
+cron.schedule('27 21 * * *', async () => {
   console.log('Running daily fee reminders...');
   try {
     const [dueToday] = await pool.query(`
